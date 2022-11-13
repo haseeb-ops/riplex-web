b83cb9784a3c6b8c2138ccc6269fa6b2f374cbf7
haseeb-ops <haseeb216633@gmail.com>
2022-11-13 23:33:36 +0530

    many changes changed

--- script.js
+++ script.js
@@ -1,45 +1,9 @@
-$(document).ready(function(){
-   $(window).scroll(function(){
-       // sticky navbar on scroll script
-       if(this.scrollY > 20){
-           $('.navbar').addClass("sticky");
-       }else{
-           $('.navbar').removeClass("sticky");
-       }
-       
-       // scroll-up button show/hide script
-       if(this.scrollY > 500){
-           $('.scroll-up-btn').addClass("show");
-       }else{
-           $('.scroll-up-btn').removeClass("show");
-       }
-   });
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
 
-   var swiper = new Swiper('.blog-slider', {
-    spaceBetween: 30,
-    effect: 'fade',
-    loop: true,
-    mousewheel: {
-      invert: false,
-    },
-    // autoHeight: true,
-    pagination: {
-      el: '.blog-slider__pagination',
-      clickable: true,
-    }
-  });
-  
-   // slide-up script
-   $('.scroll-up-btn').click(function(){
-       $('html').animate({scrollTop: 0});
-       // removing smooth scroll on slide-up button click
-       $('html').css("scrollBehavior", "auto");
-   });
-
-   $('.navbar .menu li a').click(function(){
-       // applying again smooth scroll on menu items click
-       $('html').css("scrollBehavior", "smooth");
-   });
-   
-})
-
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
